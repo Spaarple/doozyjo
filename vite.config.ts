17971acf78258048d3624313a475df001e1049bf
mlejeune <maximelj44@gmail.com>
2025-09-08 18:16:47 +0200

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/doozyjo/', // très important pour GitHub Pages
   plugins: [react()],
 })

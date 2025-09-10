3c207ed64b73151cba9ad460854aa729b67fcf07
mlejeune <maximelj44@gmail.com>
2025-09-10 10:47:49 +0200

    update: vProd - theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,12 @@ const Header: React.FC = () => {
             <a className="text-white/80 hover:text-white transition" href="/#faq">FAQ</a>
             <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" id="theme-checkbox" className="sr-only peer" />
-              <div id="theme-toggle" className="theme-toggle relative w-14 h-7 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600 dark:peer-checked:bg-gray-400">
+              <div id="theme-toggle" className="theme-toggle relative w-14 h-7 bg-gray-300 peer-focus:outline-none
+              peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer
+              dark:bg-light-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
+              peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px]
+              after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6
+              after:transition-all dark:border-gray-600 peer-checked:bg-gray-600 dark:peer-checked:bg-gray-400">
                 <div className="absolute top-1 left-1 w-5 h-5 flex items-center justify-center pointer-events-none">
                   <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"/>
@@ -93,7 +98,12 @@ const Header: React.FC = () => {
                 <label className="inline-flex items-center cursor-pointer">
                   <input type="checkbox" id="theme-checkbox" className="sr-only peer"/>
                   <div id="theme-toggle"
-                       className="theme-toggle relative w-14 h-7 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600 dark:peer-checked:bg-gray-400">
+                       className="theme-toggle relative w-14 h-7 bg-gray-300 peer-focus:outline-none peer-focus:ring-4
+                       peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-light-600
+                       peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white
+                       after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300
+                       after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600
+                       peer-checked:bg-gray-600 dark:peer-checked:bg-gray-400">
                     <div className="absolute top-1 left-1 w-5 h-5 flex items-center justify-center pointer-events-none">
                       <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 24 24">
                         <path

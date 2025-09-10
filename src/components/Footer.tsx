7cad89fd5d22abb8681be8675e21a98505c1f1eb
mlejeune <maximelj44@gmail.com>
2025-09-10 10:50:30 +0200

    update: vProd - theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Footer: React.FC = () => {
   return (
     <footer className="relative z-10 py-8 border-t border-white/10 mt-20">
-      <div className="container mx-auto px-4 text-center text-black dark:text-white/60">
-        <p>© 2025 Doozyjo. All rights reserved.</p>
+      <div className="container mx-auto px-4 text-center text-black ">
+        <p>© 2025 Doozyjo. Tous droits réservés.</p>
         <Link to="/mentions-legales">Mentions légales</Link>
       </div>
     </footer>
